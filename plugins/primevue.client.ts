bc35e19e2ed8d5c800771ab7d6c5978dfcaf9a91
Kyle Palermo <61397896+kylespalermo@users.noreply.github.com>
2025-03-30 19:21:25 -0700

    Set primevue to unstyled

---
+++ plugins/primevue.client.ts
@@ -0,0 +1,8 @@
+import PrimeVue from 'primevue/config'
+import { defineNuxtPlugin } from '#app'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(PrimeVue, {
+    unstyled: true,
+  })
+})
