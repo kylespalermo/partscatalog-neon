bb06005dfbd62fe6de16e7cc4e6249fe9b7e63d6
pburkett <pburkett636@gmail.com>
2025-11-18 14:45:26 -0700

    quick navbar cleanup

--- pages/products.vue
+++ pages/products.vue
@@ -235,51 +235,7 @@ function selectCategory(category, event) {
 </script>
 
 <template class="!bg-white">
-    <section class="w-full inner-nav">
-		 <nav class="top-nav text-white">
-      <div class="mx-auto">
-        <div class="flex items-center justify-between">
-          <!-- Brand -->
-          <div class="flex-shrink-0">
-            <span 
-              @click="$router.push('/')" 
-              class="cursor-pointer logo"
-            >
-              exo<span class="font-bold">search</span>
-            </span>
-          </div>
-
-          <!-- Desktop Menu -->
-          <div class="hidden md:flex ">
-            <a href="#" class="">All products</a>
-            <a href="#" class="">About</a>
-            <a href="#" class="">Vendor sign-in</a>
-          </div>
-
-          <!-- Mobile Toggle Button -->
-          <div class="md:hidden">
-            <button
-              @click="isOpen = !isOpen"
-              type="button"
-              class="inline-flex items-center justify-center p-2 rounded-md"
-            >
-              <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <!-- Mobile Menu -->
-      <div v-if="isOpen" class="md:hidden px-4 pb-3 space-y-2">
-        <a href="#" class="block py-2 hover:text-yellow-400">All products</a>
-        <a href="#" class="block py-2 hover:text-yellow-400">About</a>
-        <a href="#" class="block py-2 hover:text-yellow-400">Vendor sign-in</a>
-      </div>
-    </nav>
-		</section>
+    <MainNavbar />
 		<section class="inner-conatiner">
 		<div class="flex mt-[-13px] mb-[10px]">
 		 <a @click.prevent="goBack" class="flex items-center text-blue-600 hover:underline cursor-pointer float-right">
