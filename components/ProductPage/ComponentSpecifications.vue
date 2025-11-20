b593c2f0e0fe8a6bd3bedd9e10394b6c06723147
pburkett <pburkett636@gmail.com>
2025-11-20 14:52:17 -0700

    hide duplicate table

--- components/ProductPage/ComponentSpecifications.vue
+++ components/ProductPage/ComponentSpecifications.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import columnConfigurations from './column_configurations'
+import columnConfigurations from "./column_configurations";
 const props = defineProps({
   product: Object,
 });
@@ -7,10 +7,10 @@ const props = defineProps({
 let allProductProperties = {
   ...props.product,
   ...props.product.features,
-  ...props.product.application
-}
-delete allProductProperties.features
-delete allProductProperties.application
+  ...props.product.application,
+};
+delete allProductProperties.features;
+delete allProductProperties.application;
 
 const categorizedProductData = Object.values(
   columnConfigurations.reduce((acc, col) => {
@@ -19,35 +19,41 @@ const categorizedProductData = Object.values(
     if (!acc[cat]) {
       acc[cat] = {
         category_name: cat,
-        columns: []
+        columns: [],
       };
     }
     // if (allProductProperties[col.column_name]){
-      acc[cat].columns.push({
-        column_name: col.column_name,
-        display_name: col.display_name,
-        value: allProductProperties[col.column_name]
-      });
+    acc[cat].columns.push({
+      column_name: col.column_name,
+      display_name: col.display_name,
+      value: allProductProperties[col.column_name],
+    });
     // }
 
     return acc;
   }, {})
 );
-
-
 </script>
 <template>
   <div class="mt-[70px]">
     <h3 class="font-bold mb-4">Component specifications</h3>
-    <ProductPageHorizontalTable :data="categorizedProductData.find((category) => category.category_name === 'Specifications')" />
+    <ProductPageHorizontalTable
+      :data="
+        categorizedProductData.find(
+          (category) => category.category_name === 'Specifications'
+        )
+      "
+    />
     <div class="component-grid">
-      <ProductPageVerticalTable 
-              v-for="category in categorizedProductData"
-              :key="category.category_name"
-              :data="category"
-              
-              />
-      
+      <template
+        v-for="category in categorizedProductData"
+        :key="category.category_name"
+      >
+        <ProductPageVerticalTable
+          v-if="category.category_name !== 'Specifications'"
+          :data="category"
+        />
+      </template>
     </div>
   </div>
 </template>
