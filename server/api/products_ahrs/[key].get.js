fc0be13fd8b5b5db47d94e923276c389faed18df
pburkett <pburkett636@gmail.com>
2025-12-11 16:11:22 -0700

    add ahrs

---
+++ server/api/products_ahrs/[key].get.js
@@ -0,0 +1,12 @@
+export default defineEventHandler(async (event) => {
+  const productKey = getRouterParam(event, "key");
+  
+  try {
+    const result = await globalThis.sql`
+  SELECT * FROM products_ahrs WHERE key = ${productKey}
+`;
+    return { products: result[0] };
+  } catch (error) {
+    return { error: true, message: error.message };
+  }
+});
